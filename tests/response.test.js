a34476b2137ec6a4b6a173940589f767a516898a
James Speirs <james.speirs@gmail.com>
2016-10-05 16:58:55 -0600

    finish promise paradigm tests

--- tests/response.test.js
+++ tests/response.test.js
@@ -16,10 +16,13 @@
  **/
 'use strict';
 const expect            = require('chai').expect;
+const Promise           = require('bluebird');
 const Scather           = require('../index');
 
 const mock              = Scather.mock;
 const response          = Scather.response;
+
+Promise.onPossiblyUnhandledRejection(noop);
 
 describe('Scather.response', function() {
     
@@ -38,17 +41,35 @@ describe('Scather.response', function() {
     describe('returned function', function() {
         
         describe('callback paradigm', function() {
+
+            function conflict(message, attributes, callback) {
+                callback(Error('An error occurred'), message);
+            }
+
+            function echo(message, attributes, callback) {
+                callback(null, message);
+            }
+
+            function error(message, attributes, callback) {
+                callback(Error('An error occurred'), null);
+            }
+
+            function uncaught(message, attributes, callback) {
+                throw Error('Uncaught');
+                callback(null, message);
+            }
+            
             
             it('returns nothing', function() {
                 const fn = response(echo);
-                const e = mock.snsEvent('callbackArn', 'Hello');
+                const e = mock.requestEvent('callbackArn', 'Hello');
                 const result = fn(e, { functionName: 'echo' }, noop);
                 expect(result).to.equal(undefined);
             });
 
             it('gets response through callback', function() {
                 const fn = response(echo);
-                const e = mock.snsEvent('callbackArn', 'Hello');
+                const e = mock.requestEvent('callbackArn', 'Hello');
                 fn(e, { functionName: 'echo' }, function(err, data) {
                     expect(err).to.equal(null);
                     expect(data).to.equal('Hello');
@@ -57,7 +78,7 @@ describe('Scather.response', function() {
 
             it('gets error through callback', function() {
                 const fn = response(error);
-                const e = mock.snsEvent('callbackArn', 'Hello');
+                const e = mock.requestEvent('callbackArn', 'Hello');
                 fn(e, { functionName: 'error' }, function(err, data) {
                     expect(err).to.be.instanceOf(Error);
                     expect(data).to.equal(null);
@@ -66,7 +87,7 @@ describe('Scather.response', function() {
 
             it('error nullifies data', function() {
                 const fn = response(conflict);
-                const e = mock.snsEvent('callbackArn', 'Hello');
+                const e = mock.requestEvent('callbackArn', 'Hello');
                 fn(e, { functionName: 'conflict' }, function(err, data) {
                     expect(err).to.be.instanceOf(Error);
                     expect(data).to.equal(null);
@@ -75,7 +96,7 @@ describe('Scather.response', function() {
 
             it('uncaught errors are caught', function() {
                 const fn = response(uncaught);
-                const e = mock.snsEvent('callbackArn', 'Hello');
+                const e = mock.requestEvent('callbackArn', 'Hello');
                 fn(e, { functionName: 'uncaught' }, function(err, data) {
                     expect(err).to.be.instanceOf(Error);
                     expect(data).to.equal(null);
@@ -85,7 +106,54 @@ describe('Scather.response', function() {
         });
         
         describe('promise paradigm', function() {
+
+            function echo(message, attributes) {
+                return message;
+            }
+
+            function error(message, attributes) {
+                return Promise.reject(Error('An error occurred'));
+            }
+
+            function uncaught(message, attributes) {
+                throw Error('Uncaught');
+                return message;
+            }
             
+            
+            it('returns a promise', function() {
+                const fn = response(echo);
+                const e = mock.requestEvent('promiseArn', 'Hello');
+                const result = fn(e, { functionName: 'echo' }, noop);
+                expect(typeof result).to.equal('object');
+                expect(result).to.not.equal(null);
+                expect(result.then).to.be.a('function');
+            });
+
+            it('gets response through promise', function() {
+                const fn = response(echo);
+                const e = mock.requestEvent('callbackArn', 'Hello');
+                return fn(e, { functionName: 'echo' }).then(function(data) {
+                    expect(data).to.be.instanceOf(Array);
+                    expect(data[0]).to.equal('Hello');
+                });
+            });
+
+            it('gets error through callback', function() {
+                const fn = response(error);
+                const e = mock.requestEvent('callbackArn', 'Hello');
+                return fn(e, { functionName: 'error' }).then(uncaught, function(err) {
+                    expect(err).to.be.instanceOf(Error);
+                });
+            });
+
+            it('uncaught errors are caught', function() {
+                const fn = response(uncaught);
+                const e = mock.requestEvent('callbackArn', 'Hello');
+                return fn(e, { functionName: 'uncaught' }).then(uncaught, function(err, data) {
+                    expect(err).to.be.instanceOf(Error);
+                });
+            });
         });
         
         
@@ -93,21 +161,4 @@ describe('Scather.response', function() {
     
 });
 
-function conflict(message, attributes, callback) {
-    callback(Error('An error occurred'), message);
-}
-
-function echo(message, attributes, callback) {
-    callback(null, message);
-}
-
-function error(message, attributes, callback) {
-    callback(Error('An error occurred'), null);
-}
-
 function noop() {}
-
-function uncaught(message, attributes, callback) {
-    throw Error('Uncaught');
-    callback(null, message);
-}
