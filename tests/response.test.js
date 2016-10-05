533d890d6f7c73714083b08aefd0e1f94fc4539d
James Speirs <james.speirs@gmail.com>
2016-10-05 17:20:02 -0600

    fix async tests for callback paradigm

--- tests/response.test.js
+++ tests/response.test.js
@@ -67,39 +67,43 @@ describe('Scather.response', function() {
                 expect(result).to.equal(undefined);
             });
 
-            it('gets response through callback', function() {
+            it('gets response through callback', function(done) {
                 const fn = response(echo);
                 const e = mock.requestEvent('callbackArn', 'Hello');
                 fn(e, { functionName: 'echo' }, function(err, data) {
                     expect(err).to.equal(null);
-                    expect(data).to.equal('Hello');
+                    expect(data[0]).to.equal('Hello');
+                    done();
                 });
             });
 
-            it('gets error through callback', function() {
+            it('gets error through callback', function(done) {
                 const fn = response(error);
                 const e = mock.requestEvent('callbackArn', 'Hello');
                 fn(e, { functionName: 'error' }, function(err, data) {
                     expect(err).to.be.instanceOf(Error);
                     expect(data).to.equal(null);
+                    done();
                 });
             });
 
-            it('error nullifies data', function() {
+            it('error nullifies data', function(done) {
                 const fn = response(conflict);
                 const e = mock.requestEvent('callbackArn', 'Hello');
                 fn(e, { functionName: 'conflict' }, function(err, data) {
                     expect(err).to.be.instanceOf(Error);
                     expect(data).to.equal(null);
+                    done();
                 });
             });
 
-            it('uncaught errors are caught', function() {
+            it('uncaught errors are caught', function(done) {
                 const fn = response(uncaught);
                 const e = mock.requestEvent('callbackArn', 'Hello');
                 fn(e, { functionName: 'uncaught' }, function(err, data) {
                     expect(err).to.be.instanceOf(Error);
                     expect(data).to.equal(null);
+                    done();
                 });
             });
             
