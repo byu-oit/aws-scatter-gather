92a6dee05c27a51aef2b768e676a9990e7e7703a
James Speirs <james.speirs@gmail.com>
2016-10-05 17:57:17 -0600

    first aggregator test

---
+++ tests/aggregator.test.js
@@ -0,0 +1,36 @@
+/**
+ *  @license
+ *    Copyright 2016 Brigham Young University
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ **/
+'use strict';
+const expect            = require('chai').expect;
+const Promise           = require('bluebird');
+const Scather           = require('../index');
+
+const aggregator        = Scather.aggregator;
+const mock              = Scather.mock;
+
+Promise.onPossiblyUnhandledRejection(noop);
+
+describe('Scather.aggregator', function() {
+
+    it('returns a function', function() {
+        const fn = aggregator({ topicArn: 'echo' });
+        expect(fn).to.be.a('function');
+    });
+
+});
+
+function noop() {}
